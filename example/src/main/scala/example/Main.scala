196067073dc722f4819ac9a8775a47ed1a3d9af5
Venugopal Allenki <venugopal.allenki@oracle.com>
2020-09-10 16:45:55 +0530

    coursera assignments completed

---
+++ example/src/main/scala/example/Main.scala
@@ -0,0 +1,8 @@
+package example
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(Lists.max(List(12, 34, 333, 434, 986, 55, 444)))
+    println(Lists.sum(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+  }
+}
